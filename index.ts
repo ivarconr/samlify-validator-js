49d6692f4b64aa0bc01c7888aa3ed7ec0f5301b3
tngan <tonynwk919@gmail.com>
2019-06-26 03:38:56 +0900

    Export without default for commonjs

--- index.ts
+++ index.ts
@@ -18,24 +18,21 @@ schemaAssert = schemaAssert.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
 schemaAssert = schemaAssert.replace('xenc-schema.xsd', 'file_2.xsd');
 schemaXenc = schemaXenc.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
 
-export default {
+export const validate = (xml: string) => {
 
-  validate: (xml: string) => {
+  return new Promise((resolve, reject) => {
 
-    return new Promise((resolve, reject) => {
-
-      const validationResult = mod.validateXML({
-        xml: xml,
-        schema: [schemaAssert, schemaXmldsig, schemaXenc, schemaProto]
-      });
-
-      if (!validationResult.errors) {
-        return resolve('SUCCESS_VALIDATE_XML');
-      }
-
-      console.error(`this is not a valid saml response with errors: ${validationResult.errors}`);
-      return reject('ERR_EXCEPTION_VALIDATE_XML');
-
+    const validationResult = mod.validateXML({
+      xml: xml,
+      schema: [schemaAssert, schemaXmldsig, schemaXenc, schemaProto]
     });
-  }
+
+    if (!validationResult.errors) {
+      return resolve('SUCCESS_VALIDATE_XML');
+    }
+
+    console.error(`this is not a valid saml response with errors: ${validationResult.errors}`);
+    return reject('ERR_EXCEPTION_VALIDATE_XML');
+  });
+  
 };
