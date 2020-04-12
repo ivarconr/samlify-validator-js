f1874b25c951bfc184236bb97331a6ac12e0b2fc
tngan <tonynwk919@gmail.com>
2020-04-12 11:19:13 +0800

    Use const instead of let

--- index.ts
+++ index.ts
@@ -6,9 +6,9 @@ import sig from './schemas/xmldsig-core-schema.xsd';
 import xenc from './schemas/xenc-schema.xsd';
 
 // file fix for virtual filesystem of emscripten
-let schemaProto = xsd.replace('saml-schema-assertion-2.0.xsd', 'file_0.xsd').replace('xmldsig-core-schema.xsd', 'file_1.xsd');
-let schemaAssert = assertion.replace('xmldsig-core-schema.xsd', 'file_1.xsd').replace('xenc-schema.xsd', 'file_2.xsd');
-let schemaXenc = xenc.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
+const schemaProto = xsd.replace('saml-schema-assertion-2.0.xsd', 'file_0.xsd').replace('xmldsig-core-schema.xsd', 'file_1.xsd');
+const schemaAssert = assertion.replace('xmldsig-core-schema.xsd', 'file_1.xsd').replace('xenc-schema.xsd', 'file_2.xsd');
+const schemaXenc = xenc.replace('xmldsig-core-schema.xsd', 'file_1.xsd');
 
 export const validate = (xml: string) => {
   return new Promise((resolve, reject) => {
